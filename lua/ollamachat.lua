909182bcf156f35e2e88b7176993fb5237f0ecb9
Chris Kellum <christopherk379@gmail.com>
2026-01-05 21:39:41 -0500

    experiments with the function (not sure it is working)

--- lua/ollamachat.lua
+++ lua/ollamachat.lua
@@ -29,13 +29,27 @@ local grab_buffer = function()
         local buffer = vim.api.nvim_buf_get_lines(buf_nbr, 0, vim.api.nvim_buf_line_count(buf_nbr), false)
         return table.concat(buffer, "\n")
     end
-
     return {}
 end
 
-local function prompt(url)
+local function join_table(tbl)
+    print(tbl)
+    local val_tbl = tbl or {}
+    local str = ""
+    for _, value in ipairs(val_tbl) do
+        str = str .. value .. "\n"
+    end
+    print(str)
+    return str
+end
+
+local function prompt(url, captured_text)
     local prompt_text = grab_buffer()
     vim.api.nvim_put({ "", "" }, "l", true, true)
+    local captured = join_table(captured_text)
+    if not captured == nil then
+        prompt_text = "section in question:\n" .. captured .. "\n" .. prompt_text
+    end
     curl.post({
         url = url,
         stream = function(_, chunk)
@@ -43,14 +57,7 @@ local function prompt(url)
                 local res = vim.json.decode(chunk)
                 if res.error then print(res.error) end
                 if not res.done then
-                    vim.schedule(function()
-                        local response = {}
-                        if #response == 0 then
-                            local response_str = res.response:match("\0")
-                            table.insert(response, response_str)
-                        end
-                        vim.api.nvim_put(response, "", true, true)
-                    end)
+                    vim.schedule(function() vim.api.nvim_put({ res.response }, "", true, true) end)
                 else
                     vim.schedule(function()
                         vim.api.nvim_put({ "____________________" }, "l", true, true)
@@ -65,21 +72,22 @@ local function prompt(url)
     })
 end
 
-local function main(url)
+local function main(url, captured_text)
     make_window()
     vim.api.nvim_buf_set_keymap(0, "n", "<space><space>", "", {
-        callback = (function() prompt(url) end)
+        callback = (function()
+            prompt(url, captured_text)
+        end)
     })
 end
 
 M.setup = function(opts)
     opts = opts or {} -- defaults to empty table
     local url = opts.url or "http://127.0.0.1:11434/api/generate"
-    vim.api.
-        nvim_create_user_command(
-            "OllamaChat", (function()
-                main(url)
-            end), {})
+    vim.api.nvim_create_user_command("OllamaChat", (function(args)
+        print(args.fargs)
+        main(url, args.fargs)
+    end), { range = true })
 end
 
 return M
